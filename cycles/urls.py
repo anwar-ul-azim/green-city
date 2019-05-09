d84d319738c17a4a2249cebc120e080b31ea6c02
anwar-ul-azim <anwar.bhuiyan@northsouth.edu>
2019-05-09 17:16:24 +0600

    route update

--- cycles/urls.py
+++ cycles/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/', views.create, name='create'),
+    path('create/', views.create, name='createCycle'),
     path('dropcycle/', views.dropcycle, name='dropcycle'),
     path('pickcycle/', views.pickcycle, name='pickcycle'),
 ]
