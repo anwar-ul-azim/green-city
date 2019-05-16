26a0858f4b4a50eeae16322ccaa7fcd15365fc55
anwar-ul-azim <anwar.bhuiyan@northsouth.edu>
2019-05-16 18:59:23 +0600

    pagination for cycle and post

--- bicyclesharing/views.py
+++ bicyclesharing/views.py
@@ -1,8 +1,15 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from cycles.models import Cycle
 
 def home(request):
-    content = {'cycles': Cycle.objects.all()}
+    cycles_list = Cycle.objects.all()
+    paginator = Paginator(cycles_list, 5)  # Show 5 items per page
+    page = request.GET.get('page')
+    cycles = paginator.get_page(page)
+    content = {
+        'cycles': cycles
+        }
     return render(request, 'home.html', content)
 
 
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from .models import Post
 from .forms import NewPostForm
 from django.contrib import messages
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
@@ -33,5 +34,11 @@ def detail(request, id):
 
 
 def posts(request):
-    content = {'posts': Post.objects.all()}
+    posts_list = Post.objects.all()
+    paginator = Paginator(posts_list, 5)  # Show 5 items per page
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+    content = {
+        'posts': posts
+        }
     return render(request, 'posts/post.html', content)
