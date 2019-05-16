591e2a96a75ed2296fe63bec4e776de74bfa28d0
anwar <anwar.bhuiyan@northsouth.edu>
2019-05-17 03:52:36 +0600

    Post update delete option added

--- posts/views.py
+++ posts/views.py
@@ -24,6 +24,34 @@ def createPost(request):
 
 
 @login_required
+def update(request, id):
+    post = Post.objects.get(pk=id)
+    if request.user == post.author:
+        if request.method == 'POST':
+            new_post = NewPostForm(
+                request.POST, request.FILES, instance=post)
+            if new_post.is_valid():
+                new_post.save()
+                messages.success(request, f'Post update successful!')
+            return redirect('profile')
+        else:
+            content = {
+                'form': NewPostForm(instance=post),
+                'state': "update"
+            }
+            return render(request, 'posts/create.html', content)
+
+
+@login_required
+def delete(request, id):
+    post = Post.objects.get(pk=id)
+    if request.user == post.author:
+        post.delete()
+        messages.success(request, f'Post removed successfully!')
+    return redirect('profile')
+
+
+@login_required
 def detail(request, id):
     try:
         post = Post.objects.get(pk=id)
