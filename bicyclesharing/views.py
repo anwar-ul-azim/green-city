710ddda46c40fa9a8365271c872e3faad34cb101
anwar-ul-azim <anwar.bhuiyan@northsouth.edu>
2019-05-18 18:06:54 +0600

    cycle search option

--- bicyclesharing/views.py
+++ bicyclesharing/views.py
@@ -3,7 +3,16 @@ from django.shortcuts import render
 from cycles.models import Cycle
 
 def home(request):
-    cycles_list = Cycle.objects.all()
+    cycles_list = []
+    if request.method == 'POST':
+        if request.POST['search'] != "":     
+            for cycle in Cycle.objects.all():
+                if request.POST['search'] in cycle.name:
+                    cycles_list.append(cycle)
+                if request.POST['search'] in cycle.model:
+                    cycles_list.append(cycle)
+    else:
+        cycles_list = Cycle.objects.all()
     paginator = Paginator(cycles_list, 5)  # Show 5 items per page
     page = request.GET.get('page')
     cycles = paginator.get_page(page)
