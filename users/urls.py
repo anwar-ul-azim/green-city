b2a1f8c94caad97a2602908383bf541bb1f87bd4
Pias Paul <paul.pias@northsouth.edu>
2019-05-20 12:53:11 +0600

    user app url file modified to route activation link

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-
+from django.conf.urls import url
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('profile/verify/', views.profileVerify, name='profileVerify'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
 ]
